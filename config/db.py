caaf744d0a9eda18772c048f48fec4ba54b7fa4f
Yusurus <149972773+Yusurus@users.noreply.github.com>
2025-06-05 00:54:40 -0500

    cambie la ruta de los rostros_conocidos

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import os
 
-DIRECTORIO_ROSTROS = "rostros_conocidos"
+DIRECTORIO_ROSTROS = "data/rostros_conocidos"
 
 def get_connection():
     return mysql.connector.connect(
